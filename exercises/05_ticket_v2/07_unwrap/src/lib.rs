0715bf5ae7b74f87a764281492c3e291c9d40288
Raphael Sant'Anna <gomes.rsantanna@gmail.com>
2024-05-17 09:13:02 -0300

    Solutions for 05_ticket_v2

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,15 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description, status.clone()) {
+        Ok(ticket) => ticket,
+        Err(reason) => match reason.as_str() {
+            "Description cannot be empty" | "Description cannot be longer than 500 characters" => {
+                easy_ticket(title, "Description not provided".to_string(), status)
+            }
+            _ => panic!("{reason}"),
+        },
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
